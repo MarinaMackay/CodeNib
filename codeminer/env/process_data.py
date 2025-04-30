e8b74d8f36f3a6262ba60bd1eb2f1603bc60c7fc
fishmingyu <1661342068@qq.com>
2025-04-30 02:37:13 -0700

    [Update] swebench data download

--- codeminer/env/process_data.py
+++ codeminer/env/process_data.py
@@ -1,10 +1,95 @@
+import argparse
 import os
+import re
 import subprocess
+from pathlib import Path
 from typing import Any, Dict
+
+import datasets
+from datasets import Features, Value
 
 from ..log_utils import get_logger
 
 logger = get_logger(__name__)
+
+
+def load_filter_swebench_dataset(
+    args: argparse.Namespace,
+) -> datasets.arrow_dataset.Dataset:
+    ret = load_filter_swebench_dataset_explicit(
+        dataset=args.dataset, filter_instance=args.filter_instance, split=args.split
+    )
+    # Cannot has both idx_list and idx_range
+    assert not (
+        hasattr(args, "idx_list") and hasattr(args, "idx_range")
+    ), "Cannot has both idx_list and idx_range in arguments"
+    if hasattr(args, "idx_list"):
+        if args.filter_instance != ".*":
+            logger.info(
+                (
+                    "Running idx_list on a filtered (non-full) dataset."
+                    "Please make sure this is expected."
+                )
+            )
+        return ret.select(args.idx_list)
+    elif hasattr(args, "idx_range"):
+        if args.filter_instance != ".*":
+            logger.info(
+                (
+                    "Running idx_range on a filtered (non-full) dataset."
+                    "Please make sure this is expected."
+                )
+            )
+        start_idx = args.idx_range[0]
+        end_idx = args.idx_range[1]
+        assert start_idx < end_idx, "start_idx should be smaller than end_idx"
+        return ret.select(range(start_idx, end_idx))
+    else:
+        return ret
+
+
+def load_filter_swebench_dataset_explicit(
+    dataset: str, filter_instance: str, split: str
+) -> datasets.arrow_dataset.Dataset:
+    cache_dir = str(Path.home()) + "/.codeminer"
+    # Create cache directory if it doesn't exist
+    cache_dir = os.path.abspath(cache_dir)
+    if not os.path.exists(cache_dir):
+        logger.info(f"Creating cache directory at {cache_dir}")
+        os.makedirs(cache_dir, exist_ok=True)
+    dataset_file = f'{dataset.replace("/", "__")}_{split}.json'
+    dataset_path = f"{cache_dir}/{dataset_file}"
+    if not os.path.exists(dataset_path):
+        ds = datasets.load_dataset(dataset, split=split)
+        logger.info(f"Loaded {len(ds)} instances from {dataset} dataset, split {split}")
+        ds.to_json(dataset_path)
+    else:
+        logger.info(f"Dataset already exists at {dataset_path}")
+        data_files = {split: dataset_path}
+        ft = Features(
+            {
+                "repo": Value("string"),
+                "instance_id": Value("string"),
+                "base_commit": Value("string"),
+                "patch": Value("string"),
+                "test_patch": Value("string"),
+                "problem_statement": Value("string"),
+                "hints_text": Value("string"),
+                "created_at": Value("string"),
+                "version": Value("string"),
+                "FAIL_TO_PASS": Value("string"),
+                "PASS_TO_PASS": Value("string"),
+                "environment_setup_commit": Value("string"),
+            }
+        )
+        ds = datasets.load_dataset(
+            "json", data_files=data_files, split=split, features=ft
+        )
+        logger.info(f"Loaded {len(ds)} instances from cached dataset at {dataset_path}")
+    return ds.filter(
+        input_columns=["instance_id"],
+        function=lambda x: bool(re.match(filter_instance, x)),
+    )
 
 
 def process_swebench_instance(
