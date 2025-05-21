f11980c03563a0e28d7dd6bf2538d17a697b2ea1
fishmingyu <1661342068@qq.com>
2025-05-21 11:37:49 -0700

    [Update] better type system for codegraph

--- test/test_codegraph.py
+++ test/test_codegraph.py
@@ -1,8 +1,11 @@
-from codeminer.scip_interface import SCIPIndexer
-from codeminer.env.process_data import load_filter_swebench_dataset, process_swebench_instance
 import argparse
 from pathlib import Path
 
+from codeminer.env.process_data import (
+    load_filter_swebench_dataset,
+    process_swebench_instance,
+)
+from codeminer.scip_interface import SCIPIndexer
 
 args_dict = {
     "model": "gpt-4o",
@@ -23,12 +26,10 @@ if __name__ == "__main__":
         )
         print(f"Base commit: {instance['base_commit']}")
         print(f"Problem statement: {instance['problem_statement']}")
-        repo_path = process_swebench_instance(
-            instance
-        )
+        repo_path = process_swebench_instance(instance)
         # set output path with ~/.codeminer/instance_id
         output_path = str(Path.home()) + "/.codeminer/" + instance["instance_id"]
-        
+
         # setup codegraph
         repo_indexer = SCIPIndexer(repo_path, output_dir=output_path)
 
@@ -39,7 +40,7 @@ if __name__ == "__main__":
 
         # get node info
         # node name = astropy.modeling.separable/separability_matrix().
-        attributes = graph.get_node_info(
+        attributes = graph.get_node_info_by_name(
             node_name="astropy.modeling.separable/separability_matrix()."
         )
         print(f"Node attributes: {attributes}")
