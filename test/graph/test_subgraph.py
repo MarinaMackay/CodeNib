ba7e9372c5e527b14b45f471301fce7445b7d2cf
fishmingyu <1661342068@qq.com>
2025-09-23 23:17:20 -0700

    [Test] fix bug for roi_rerank

---
+++ test/graph/test_subgraph.py
@@ -0,0 +1,70 @@
+import argparse
+from pathlib import Path
+
+from codeminer.env.process_data import (
+    load_filter_swebench_dataset,
+    process_swebench_instance,
+)
+from codeminer.graph.roi_subgraph import ROISubgraph
+from codeminer.scip_interface import SCIPIndexer
+from codeminer.sparse_idx.bm25_index import BM25CodeIndexer
+
+args_dict = {
+    "model": "gpt-4o",
+    "dataset": "princeton-nlp/SWE-bench_Lite",
+    "split": "test",
+    "filter_instance": "^(astropy__astropy-12907)$",
+}
+
+
+# Example usage
+if __name__ == "__main__":
+    # load instance from command line
+    args = argparse.Namespace(**args_dict)
+    dataset = load_filter_swebench_dataset(args=args)
+    for _, instance in enumerate(dataset):
+        print(
+            f"Loaded instance: {instance['instance_id']} from repo {instance['repo']}"
+        )
+        print(f"Base commit: {instance['base_commit']}")
+        print(f"Problem statement: {instance['problem_statement']}")
+        repo_path = process_swebench_instance(instance)
+        # set output path with ~/.codeminer/instance_id
+        output_path = str(Path.home()) + "/.codeminer/" + instance["instance_id"]
+
+        # setup codegraph
+        repo_indexer = SCIPIndexer(repo_path, output_dir=output_path)
+
+        # Run the indexing pipeline, allowing skip_index and skip_decode for faster tests
+        graph = repo_indexer.run_pipeline(
+            project_name="test_swebench",
+        )
+
+        # get node info
+        node_name = "astropy.modeling.separable/separability_matrix()."
+
+        # Create BM25 indexer with English stemming for method name matching
+        indexer = BM25CodeIndexer(max_k=10, language="english")
+
+        # Build the index from the code graph
+        indexer.build_index_from_graph(graph)
+        # Search for the node name
+        print(f"Searching for node name: {node_name}")
+        results = indexer.search(node_name, top_k=5)
+        print(f"Search results: {results}")
+        # Extract node names from search results
+        node_names = [result.node_name for result in results]
+        print(f"Node names: {node_names}")
+        # Create ROISubgraph object
+        roi_subgraph = ROISubgraph(graph)
+        # Extract subgraph with k-hop neighbors
+        k_hop = 1
+        subgraph = roi_subgraph.extract_subgraph(
+            [node_names[0]], k_hop, direction="downward"
+        )
+        # get filtered subgraph nodes
+        filtered_nodes = roi_subgraph.get_filtered_subgraph_nodes(
+            subgraph, exclude_nodes=[node_names[0]], filter_tests=True
+        )
+        for node in filtered_nodes:
+            print(f"Node ID: {node.node_name}, Type: {node.type}")
