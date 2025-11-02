42701204ea5943108615e89c1ae73046bce365fd
fishmingyu <1661342068@qq.com>
2025-11-01 22:18:12 -0700

    [Test] add swebench scip test

---
+++ test/scip/test_scip_swebench.py
@@ -0,0 +1,33 @@
+import argparse
+from pathlib import Path
+
+from codeminer.env import load_filter_swebench_dataset, process_swebench_instance
+from codeminer.scip_interface import SCIPIndexer
+
+args_dict = {
+    "model": "gpt-4o",
+    "dataset": "princeton-nlp/SWE-bench_Lite",
+    "split": "test",
+    "filter_instance": "^(psf__requests-1963)$",
+}
+
+
+def test_scip_exclude():
+    # exclude_file = "sympy/polys/numberfields/resolvent_lookup.py"
+    exclude_pattern = "test_*"
+    args = argparse.Namespace(**args_dict)
+    dataset = load_filter_swebench_dataset(args=args)
+
+    instance = dataset[0]
+    repo_path = process_swebench_instance(instance)
+    # set output path with ~/.codeminer/instance_id
+    output_path = str(Path.home()) + "/.codeminer/" + instance["instance_id"]
+    # setup codegraph with exclude patterns
+    repo_indexer = SCIPIndexer(
+        repo_path,
+        output_dir=output_path,
+        exclude_patterns=[exclude_pattern],
+    )
+
+    # Run the indexing pipeline from scratch (skip_level=None)
+    repo_indexer.run_pipeline(project_name="test_swebench", skip_level="index")
